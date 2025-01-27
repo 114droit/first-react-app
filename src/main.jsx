989e4cbcc75cc7c45ab8f630aed5e1a211f870bc
144droit <indieohren@gmail.com>
2025-01-27 17:18:23 +0100

    deleted App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './pages/App.jsx'
+import Header from './components/Header';
+import Footer from './components/Footer';
+import ProductGallery from './components/ProductGallery';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Header />
+    <ProductGallery />
+    <Footer />
   </StrictMode>,
 );
